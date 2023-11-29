c3a9620e62b97b2ba5f553eb6041c543180843eb
PieterNatanael <pieternatanael@icloud.com>
2023-11-29 14:12:00 +0800

    Initial Commit

---
+++ LoopSpeak/LoopSpeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopSpeakApp.swift
+//  LoopSpeak
+//
+//  Created by Pieter Yoshua Natanael on 29/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LoopSpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
